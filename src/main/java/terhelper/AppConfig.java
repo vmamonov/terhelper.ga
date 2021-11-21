af076935f3720b66b5bc5711b34f910aaefed2bf
Mamonov Victor <37188975+vmamonov@users.noreply.github.com>
2021-11-21 12:31:44 +0100

    edit path to config

--- src/main/java/terhelper/AppConfig.java
+++ src/main/java/terhelper/AppConfig.java
@@ -18,7 +18,7 @@ public class AppConfig {
 	
 	public AppConfig()  {
 		try {
-			pathToConfigFile = "/sites/terhelper/src/main/java/terhelper/app-config.xml";
+			pathToConfigFile = "/apps/terhelper/app-config.xml";
 			config = DocumentBuilderFactory
 				.newInstance()
 				.newDocumentBuilder()
